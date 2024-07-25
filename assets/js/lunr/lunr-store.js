9deb1d41b94f6304725f7e67ee1ed9c84c83a579
romain-jacob <romain-jacob@users.noreply.github.com>
2024-07-25 09:20:46 +0000

    jekyll build from Action ec237d18590d131e6d6e39337cbe57ea4d795f90

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,115 @@
+var store = [{
+        "title": "My Thesis in 180s",
+        "excerpt":"In 2016, I participated in an internal \"My Thesis in 180s\" organized by Nano-Tera.ch, a Swiss initiative that founded multiple cutting-edge research projects, including X-Sense2, in which my group was involved. This performance tells the story of the PermaSense project, which deploys wireless sensor networks in the mountains to enable...","categories": [],
+        "tags": [],
+        "url": "/2016/05/01/NanoTera.html",
+        "teaser": "/assets/images/Noto_Romain.png"
+      },{
+        "title": "Famelab - Future is coming",
+        "excerpt":"            In 2017, I participated in Famelab, an international science communication competition similar to the increasingly popular \"My thesis in 180s\" contest.           In this performance, entitled Future is coming,  I discuss how artificial intelligence and machine learning are invading our everyday lives.           (Famelab Swiss Finals, Basel,  June 2017)                                               ","categories": [],
+        "tags": [],
+        "url": "/2017/06/01/Famelab.html",
+        "teaser": "/assets/images/Noto_Romain.png"
+      },{
+        "title": "Internet of Things - The \"smart-everything\" era",
+        "excerpt":"       Over Summer 2018, I followed a great training from the MAZ school of journalism from Luzern.           In this video, I present some of the challenges in having more and more wireless connected objects, and how we have been using techniques like 'Synchronous Transmissions' to mitigate some of these problems.                 ","categories": [],
+        "tags": [],
+        "url": "/2018/09/01/SmartEverything.html",
+        "teaser": "/assets/images/Noto_Romain.png"
+      },{
+        "title": "The eMedusa",
+        "excerpt":"Smart objects are part of our lives. Everything gets connected and continuously exchanges information. More information about our tastes and habits let the objects around us provide better, tailored services. We dream of simpler and happier lives. But this may also be a daunting prospect. Aren’t fictions like 1984, or...","categories": [],
+        "tags": [],
+        "url": "/2019/01/01/eMedusa.html",
+        "teaser": "/assets/images/Noto_Romain.png"
+      },{
+        "title": "What do you (really) know about WiFi?",
+        "excerpt":"This video was created in November 2018 as part of a project for the graduate course \"Science &amp; Policy: communicating science\" at ETH Zurich, which aim to train scientists to clearly communicate scientific findings to the general public in concise, cleared and simplified manner (as we tried to do in...","categories": [],
+        "tags": [],
+        "url": "/2019/10/01/WiFi.html",
+        "teaser": "/assets/images/Noto_Romain.png"
+      },{
+        "title": "Starting a new adventure! … from the same office",
+        "excerpt":"Napping in Wadi Rum (Jordan) A few months ago, I completed my doctoral studies, which I was lucky enough to carry on at ETH Zurich, Switzerland. Now what’s next? Well, I love teaching, supervising students, and it seems I’m also not too bad at research; a career in academia seems...","categories": ["news"],
+        "tags": [],
+        "url": "/blog/2020-05-new-adventure-same-office",
+        "teaser": "/assets/images/Noto_Romain.png"
+      },{
+        "title": "Launch of JSys",
+        "excerpt":"I am thrilled to announce the launch of the new diamond open-access Journal of Systems Research (JSys)! I’m really super excited to be a part of this grand challenge 🙂 Read about it more here or visit the journal website. The first call for paper will follow soon! JSys website...","categories": ["news"],
+        "tags": [],
+        "url": "/news/2020/11/20/jsysLaunch.html",
+        "teaser": "/assets/images/Noto_Romain.png"
+      },{
+        "title": "Zukunftspreis",
+        "excerpt":"I am humbled (and very happy!) to announce that our work on “Feedback control goes wireless” has received the 2nd Zukunftspreis (Future Price) from the Ewald Marquardt foundation. In this collaboration with control researchers, we demonstrated provable stability guarantees for multiple inverted pendulums distributed over a multi-hop wireless network. The...","categories": ["news"],
+        "tags": [],
+        "url": "/news/2021/01/20/Zukunftpreis.html",
+        "teaser": "/assets/images/Noto_Romain.png"
+      },{
+        "title": "TriScale at CPS-IoTBench",
+        "excerpt":"At CPS-IoTBench 2021, I gave my first public tutorial about TriScale, our recent work aiming to foster replicability in networking experiments.   A longer version (half-day) is now in preparation for SIGCOMM 2021!                         Tutorial recording (~90min)                        TriScale paper                        Tutorial material (and more)          ","categories": ["news"],
+        "tags": [],
+        "url": "/news/2021/05/05/TriScaleCPSIoTBench.html",
+        "teaser": "/assets/images/Noto_Romain.png"
+      },{
+        "title": "Joining SwissRN",
+        "excerpt":"Happy to announce that I officially joined the Swiss Reproducibility Network, an academic-driven consortium that aims to promote and ensure rigorous research practices in Switzerland!                         SwissRN website          ","categories": ["news"],
+        "tags": [],
+        "url": "/news/2021/06/11/SwissRN.html",
+        "teaser": "/assets/images/Noto_Romain.png"
+      },{
+        "title": "TriScale Tutorial at SIGCOMM",
+        "excerpt":"Last week, I held a half-day tutorial about TriScale at ACM SIGCOMM. That was the opportunity for a new webpage, polished hands-on material, an extended slide pack, and a better video recording! Check-it out and spread the word! Let’s foster replicability in networking research! Tutorial recording (1h40) Tutorial slides TriScale...","categories": ["news"],
+        "tags": [],
+        "url": "/news/2021/08/23/TriScale-SIGCOMM.html",
+        "teaser": "/assets/images/Noto_Romain.png"
+      },{
+        "title": "Starting as Editor-in-Chief for JSys",
+        "excerpt":"It is my pleasure to serve as one of the JSys Editors-in-Chief.   Looking forward to helping bring your research to the world, with high quality review, artifact evaluation, and of course, no fees nor paywall!                         JSys website          ","categories": ["news"],
+        "tags": [],
+        "url": "/news/2021/09/01/jsysEiC.html",
+        "teaser": "/assets/images/Noto_Romain.png"
+      },{
+        "title": "Curating your professional network",
+        "excerpt":"My 2022 email picture: Me and my daughter focused on our task. As a young researcher, you are often told that “it’s important to network,” that you should know a lot of people, and that they should know you. I agree, it’s important. But this post is not about building...","categories": ["random"],
+        "tags": [],
+        "url": "/blog/2020-01-networking",
+        "teaser": "/assets/images/Noto_Romain.png"
+      },{
+        "title": "Joining the ORCID Researcher Advisory Council",
+        "excerpt":"I have been selected to serve in the very first Researcher Advisory Council of ORCID.   I hope to assist ORCID in its mission to “help (researchers) spend more time conducting their research and less time managing it.” How cool is that?                          ORCID news                        Tweet          ","categories": ["news"],
+        "tags": [],
+        "url": "/news/2022/02/02/ORAC.html",
+        "teaser": "/assets/images/Noto_Romain.png"
+      },{
+        "title": "Presentation at HotCarbon",
+        "excerpt":"I have been really happy to present our first technical paper at the 2023 HotCarbon workshop. That marks the first milestone of my journey researching “sustainable networking.” Check it out!                         Paper                        Presentation slides                        Recording                        Podcast coverage (@9:27)          ","categories": ["news"],
+        "tags": [],
+        "url": "/news/2023/07/10/HotCarbon.html",
+        "teaser": "/assets/images/Noto_Romain.png"
+      },{
+        "title": "Lecture material on Sustainable Networking",
+        "excerpt":"To wrap up 2023, I had the pleasure of giving a 2-hour lecture on Sustainable Networking for Master’s students. As always, I learned a lot while preparing to teach this class. It also made me aware of how much I’ve learned about sustainability in the past 18 months; back then,...","categories": ["news"],
+        "tags": [],
+        "url": "/news/2024/01/22/AdvNet.html",
+        "teaser": "/assets/images/Noto_Romain.png"
+      },{
+        "title": "Pledge to Open Science - 2024 review",
+        "excerpt":"In 2019, as I was approaching the end of my doctorate, I decided to commit to open science practices. Why? Can’t say it any better than Erin McKiernan, who inspired me to do this. If I am going to “make it” in science, it has to be on terms I...","categories": [],
+        "tags": [],
+        "url": "/2024/02/23/PlegeUpdate.html",
+        "teaser": "/assets/images/Noto_Romain.png"
+      },{
+        "title": "Pledge to Open Science - 2024 review",
+        "excerpt":"After 4.5 years, I finally took the time to assess my commitment to my pledge to open science. A nice opportunity to reflect on my past choices–and do some adjustments.                         Blog post          ","categories": ["news"],
+        "tags": [],
+        "url": "/news/2024/02/23/PlegeUpdate.html",
+        "teaser": "/assets/images/Noto_Romain.png"
+      },{
+        "title": "Best Paper Award at HotCarbon'24",
+        "excerpt":"For the third year in a row, we presented our ongoing sustainable networking work at HotCarbon.  Cherry on the cake: We had the pleasure of receiving the Best Paper Award! Check it out!                         Paper                        Slides                        Podcast          ","categories": ["news"],
+        "tags": [],
+        "url": "/news/2024/07/25/HotCarbon_Hypnos.html",
+        "teaser": "/assets/images/Noto_Romain.png"
+      }]
